a74a0f3f8d7f8bd7189d95cfd5e671855bff9027
Pedro Cordioli <pedrosenacmiro@hotmail.com>
2022-10-25 22:28:04 -0300

    metflix

---
+++ app.js
@@ -0,0 +1,12 @@
+let filme01 = 'imagens/filme01.jpg'
+let filme02 = 'imagens/filme02.jpg'
+let filme03 = 'imagens/filme03.jpg'
+let filme04 = 'imagens/miranha1.jpg'
+let filme05 = 'imagens/miranha2.jpg'
+let filme06 = 'imagens/miranha3.jpg'
+document.querySelector(".container").innerHTML += "<img src='" + filme01 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme02 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme03 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme04 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme05 + "'> "
+document.querySelector(".container").innerHTML += "<img src='" + filme06 + "'> "
